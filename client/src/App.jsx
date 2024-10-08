ee6eb6a05f2d7aef171c005e35ba65feb649bc95
ABHI <abhi202864@gmail.com>
2024-10-08 23:47:35 +0530

    work on navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Doctors from './pages/Doctors';
+import Login from './pages/Login';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import MyProfile from './pages/MyProfile';
+import MyAppointments from './pages/MyAppointments';
+import Appointments from './pages/Appointments';
+import NavBar from './components/NavBar';
+
 
 const App = () => {
   return (
-    <div>
-      <h1>ABHI</h1>
+    <div className="mx-4 sm:mx-[10%]">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:speciality" element={<Doctors />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/myProfile" element={<MyProfile />} />
+        <Route path="/myAppointments" element={<MyAppointments />} />
+        <Route path="/Appointments/:docId" element={<Appointments />} />
+      </Routes>
     </div>
   )
 }
