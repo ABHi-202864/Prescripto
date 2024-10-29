8e7a5b65f1172ddd6d2b78f1afa1eff7289175d2
ABHI <abhi202864@gmail.com>
2024-10-29 21:12:01 +0530

    complete MOBILE MENU

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -48,6 +48,22 @@ const NavBar = () => {
             </div>
             : <button className='bg-primary text-white px-8 py-3 rounded-full font-light md:block' onClick={() => navigate("/login")}>Create account</button>
         }
+
+
+        <img className='w-6 md:hidden' onClick={() => setShowMenu(true)} src={assets.menu_icon} alt="" />
+        {/* MOBILE MENU */}
+        <div className={`${showMenu ? "fixed w-full" : "h-0 w-0"} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+          <div className='flex items-center justify-between px-5 py-6'>
+            <img className='w-36' onClick={() => navigate("/")} src={assets.logo} alt="" />
+            <img className='w-7 ' onClick={() => setShowMenu(false)} src={assets.cross_icon} alt="" />
+          </div>
+          <ul className='flex flex-col gap-2 text-lg font-medium text-gray-700'>
+            <NavLink className="px-5 py-0.5 inline-block" onClick={() => setShowMenu(false)} to="/" ><p className='border rounded px-5'>HOME</p></NavLink>
+            <NavLink className="px-5 py-0.5 inline-block" onClick={() => setShowMenu(false)} to="/doctors"><p className='border rounded px-5'>ALL DOCTORS</p></NavLink>
+            <NavLink className="px-5 py-0.5 inline-block" onClick={() => setShowMenu(false)} to="/about"><p className='border rounded px-5'>ABOUT</p></NavLink>
+            <NavLink className="px-5 py-0.5 inline-block" onClick={() => setShowMenu(false)} to="/contact"><p className='border rounded px-5'>CONTACT</p></NavLink>
+          </ul>
+        </div>
       </div>
     </div>
   )
