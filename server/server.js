e5804c5861194c15a05e8142843ec47e43dd4c22
ABHI <abhi202864@gmail.com>
2024-11-02 22:23:01 +0530

    DB connection complete

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-
+import connectDB from "./config/mongodb.js";
 
 
 // APP CONFIG 
 const app = express();
-const port = process.env.PORT || 4000; 
+const port = process.env.PORT || 4000;
+connectDB();
 
 //MIDDLEWARES
 app.use(express.json());
+app.use(cors());
+
+//API END-POINTS
+app.get("/", (req, res) => {
+  res.send("Hello - ABHI");
+});
+
+
+
+//SERVER LISTEN 
+app.listen(port, () => console.log(`DOCKING PORT : ${port}`));
