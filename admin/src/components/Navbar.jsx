df32f7f048079a6213f88353099fa7ce0f93b00e
ABHI <abhi202864@gmail.com>
2024-11-17 23:55:03 +0530

    addmin sidebar make

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { useNavigate } from "react-router-dom";
 import { assets } from '../assets/assets_admin/assets';
 import { AdminContext } from '../context/AdminContext';
---
+++ admin/src/components/Sidebar.jsx
@@ -0,0 +1,24 @@
+import { useContext } from 'react';
+import { AdminContext } from '../context/AdminContext';
+import { NavLink } from 'react-router-dom';
+import { assets } from '../assets/assets_admin/assets';
+
+const Sidebar = () => {
+
+  const { aToken } = useContext(AdminContext);
+
+  return (
+    <div>
+      {
+        aToken && <ul>
+          <NavLink>
+            <img src={assets.home_icon} alt="" />
+            <p>Dashboard</p>
+          </NavLink>
+        </ul>
+      }
+    </div>
+  )
+}
+
+export default Sidebar;
