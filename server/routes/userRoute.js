e8083d9e6941a6822f7c4d36bf5324b5b7ccac2e
ABHI <abhi202864@gmail.com>
2024-12-04 23:16:18 +0530

    add user Profile update functionality

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import { registerUser, loginUser } from "../controllers/userController.js";
+import { registerUser, loginUser, getProfile, updateProfile } from "../controllers/userController.js";
+import authUser from "../middlewares/authUser.js";
+import upload from "../middlewares/multer.js";
+
 
 const userRouter = Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
+userRouter.post("/update-profile", upload.single("image"), authUser, updateProfile);
+
+userRouter.get("/get-profile", authUser, getProfile);
+
 
 export default userRouter;
